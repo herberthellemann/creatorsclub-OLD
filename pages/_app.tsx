7c79bd985fefe9ffc5d87924872ed91330ec092a
Herbert Hellemann <zermanik@gmail.com>
2022-05-27 16:32:23 +0200

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import myTheme from "../constants/theme";
+import "@fontsource/inter/variable.css";
+
+import "@fontsource/inter";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <ChakraProvider theme={myTheme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
